327b302da229f3b44e91044841bd2a573bb74e66
Musyimi <newtonthemaker@gmail.com>
2022-05-25 01:10:57 +0200

    Update rpslsGAME.cpp

--- rpslsGAME.cpp
+++ rpslsGAME.cpp
@@ -1,6 +1,7 @@
 /*
 This is Rock-Papers-Scissors-Lizard-Spock;a spin-off ot the original RPS game that reduces the chance of player's having a tie.
 For more, check out the README file on https://github.com/Newton-Musyimi/Learning-C-and-CPP
+compilation: `g++ rpslsGAME.cpp rpsls.cpp`
 */
 #include <cstdlib>
 #include <ctime>
